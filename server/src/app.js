95a5f28ccdfce74bd8f3bf7b1482d7bd4f170229
Ritesh Upadhyay <upadhyay07ritesh@gmail.com>
2025-11-02 16:45:14 +0530

    Changes in CORS

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:5173',methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
